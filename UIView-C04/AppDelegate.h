e3e7175af868b70b2f9449035080a2ffb510f4c1
johnbob2014 <johnbob2014@icloud.com>
2016-06-03 10:40:15 +0800

    Initial Commit

---
+++ UIView-C04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView-C04
+//
+//  Created by BobZhang on 16/6/3.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
